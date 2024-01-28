402fe884d60079adcbc1455123b32e4a761a4429
Adam El Amrani Colina <adelamco1091@outlook.com>
2024-01-28 19:27:25 +0100

    Create List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import './App.css';
+import List from './components/list/List';
+import { useGetPopularMoviesQuery } from './redux/api/moviesApi';
+import { FetchErrorTMDB } from './types/ErrorTMDB';
+import { Movies } from './types/Movies';
 
 function App() {
+  const { data, error } = useGetPopularMoviesQuery<{
+    data: Movies;
+    isLoading: boolean;
+    error: FetchErrorTMDB;
+  }>({});
+
   return (
     <>
-      <p>hi</p>
+      {error && (
+        <>
+          <p>
+            <i>There has been an error loading the</i>
+          </p>
+          <p>
+            <i>{error.data.status_message}</i>
+          </p>
+        </>
+      )}
+      <List>
+        {data?.results.map((element) => (
+          <li key={element.id}>{element.title}</li>
+        ))}
+      </List>
     </>
   );
 }
